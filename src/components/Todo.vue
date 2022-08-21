ef121446fe513c4ea029976f7007325924b23fa2
Kunal Singh <singhkunal2050@gmail.com>
2022-08-21 15:16:00 +0530

    added doc

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -13,6 +13,7 @@ export default {
 <template>
     <div
         class="todo max-w-4xl flex flex-col mx-auto my-8 bg-[#f5f5f5] min-h-[80vh] outline outline-1 outline-gray-200 shadow-lg">
+        <!-- We can pass props to all view if we pass it to router-view component -->
         <router-view></router-view>    
         <Footer/>
     </div>
