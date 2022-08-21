2e962c29e8dfee7e2e99deb38433f25415a7aef3
Kunal Singh <singhkunal2050@gmail.com>
2022-08-21 16:46:48 +0530

    Added About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="py-4 px-2 flex gap-4 flex-col items-center">
        <h2 class="text-xl font-bold">About Us</h2>
-       <router-link to="/">Go Back Home 🏠</router-link>
+
+        <img src="https://source.unsplash.com/500x500?cat" alt="Random Cat">
+        <router-link to="/">Go Back Home 🏠</router-link>
 
     </div>
 </template>
+
+<script>
+
+</script>
