99e01b07a579dc8e19aa3ba79bc88f18e40a5545
Kunal Singh <singhkunal2050@gmail.com>
2022-08-21 20:05:11 +0530

    UI Updates

--- src/components/Todo/List.vue
+++ src/components/Todo/List.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="px-5 mt-2" :key="todo.key" v-for="todo in todos">
-        <TodoItem :title="todo.title" :completed="todo.completed" :id="todo.id" :highlight="todo.highlight" @delete-todo="$emit('delete-todo',todo.id)" @toggle-highlight="$emit('toggle-highlight',todo.id)" @toggle-complete="$emit('toggle-complete',todo.id)" />
+    <div class="list-wrapper h-[50vh] overflow-auto">
+        <div class="px-5 mt-2" :key="todo.key" v-for="todo in todos">
+            <TodoItem :title="todo.title" :completed="todo.completed" :id="todo.id" :highlight="todo.highlight"
+                @delete-todo="$emit('delete-todo', todo.id)" @toggle-highlight="$emit('toggle-highlight', todo.id)"
+                @toggle-complete="$emit('toggle-complete',todo.id)" />
+        </div>
     </div>
 </template>
 <script>
 import TodoItem from './TodoItem.vue';
 export default {
     components: { TodoItem },
-    props:{
-        todos:Array
+    props: {
+        todos: Array
     },
-    emits:['delete-todo','toggle-highlight','toggle-complete']
+    emits: ['delete-todo', 'toggle-highlight', 'toggle-complete']
 }
 </script>
 <style >
-    
 </style>
