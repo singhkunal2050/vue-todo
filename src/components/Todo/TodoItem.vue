0984886bc2c54a13e3e9e62cd860783e1d91fb85
Kunal Singh <singhkunal2050@gmail.com>
2022-08-21 11:46:24 +0530

    Todo compoenent updates

--- src/components/Todo/TodoItem.vue
+++ src/components/Todo/TodoItem.vue
@@ -1,5 +1,5 @@
 <template lang="">
-        <article @dblclick="$emit('toggle-highlight',id)" :class="[ highlight ?  'border-l-red-400' : 'border-l-transparent' , completed ?  'bg-green-100 border-l-green-400' : 'bg-red-100' , , 'flex' ,'justify-between' ,'p-2' , 'py-4', 'px-2', 'bg-white', 'mx-1' , 'my-2', 'shadow-lg' , 'border-l-4']" :data-todo-id="id">
+        <article @dblclick="$emit('toggle-highlight',id)" :class="[ highlight ?  'border-l-orange-500' : 'border-l-transparent' , completed ?  'bg-green-200' : 'bg-red-200' , , 'flex' ,'justify-between' ,'p-2' , 'py-4', 'px-2', 'bg-white', 'mx-1' , 'my-2', 'shadow-lg' , 'border-l-4']" :data-todo-id="id">
             <div class="todo-content">
                 <p> {{title}}</p>
                 <span class="cursor-pointer text-xs p-2" @click="$emit('toggle-complete',id)"> {{completed ? "Completed" : "Incomplete"}}</span>
